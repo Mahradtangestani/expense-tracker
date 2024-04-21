6f871292f45b41fb4806f1c3541cc635259c43fd
mahrad <mahradti@gmail.com>
2024-04-21 20:29:43 +0330

    About Balance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import Balance from './components/Balance';
 
 function App() {
   return (
     <div>
       <Header/>
+      <div className='container'>
+          <Balance/>
+      </div>
     </div>
   );
 }
