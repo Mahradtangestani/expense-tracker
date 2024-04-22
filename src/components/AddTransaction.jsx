42a56a09b65bf88667abaedff5c9cc01311cfa5d
mahrad <mahradti@gmail.com>
2024-04-22 18:44:36 +0330

    GlobalState

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTransaction = () => {
+  
+  const [text , setText] = useState("");
+  const [amount , setAmount] = useState(0)  
+
   return (
     <>
       <h3>Add new transaction</h3>
-      <form id="form">
-        <div class="form-control">
-          <label for="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+      <form>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input type="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder="Enter text..." />
         </div>
-        <div class="form-control">
-          <label for="amount">
+        <div className="form-control">
+          <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input type="number" value={amount} onChange={(e)=>setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
-        <button class="btn">Add transaction</button>
+        <button className="btn">Add transaction</button>
       </form>
     </>
   );
--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const TransactionList = () => {
+    
+  const context = useContext(GlobalContext)
+  console.log(context);
+
     return (
         <>
           <h3>History</h3>
-          <ul id='list' className='list'>
+          <ul className='list'>
             <li className='minus'>
                 Cash <span>-$400</span><button className='delete-btn'>x</button>
             </li>
