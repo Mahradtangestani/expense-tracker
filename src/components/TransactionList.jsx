9976a2d01b78818a821368688845030c8a5b7751
mahrad <mahradti@gmail.com>
2024-04-23 19:44:16 +0330

    Dynamic Transaction

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,18 +1,20 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
+import Transaction from './Transaction';
 
 const TransactionList = () => {
     
-  const context = useContext(GlobalContext)
-  console.log(context);
+  const {transactions} = useContext(GlobalContext)
+  
 
     return (
         <>
           <h3>History</h3>
           <ul className='list'>
-            <li className='minus'>
-                Cash <span>-$400</span><button className='delete-btn'>x</button>
-            </li>
+            {transactions.map(transaction =>(
+             <Transaction key={transaction.id} transaction={transaction}/>
+            ))}
+            
           </ul>   
         </>
     );
