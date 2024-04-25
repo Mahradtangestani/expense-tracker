7c303c974bb9437b500596a6e28d6b0f8e46d576
mahrad <mahradti@gmail.com>
2024-04-25 23:40:53 +0330

    Delete Transaction

--- src/components/IncomeExpenses.jsx
+++ src/components/IncomeExpenses.jsx
@@ -6,6 +6,7 @@ const IncomeExpenses = () => {
   const { transactions } = useContext(GlobalContext);
 
   const amount = transactions.map(transaction => transaction.amount);
+  
   const income = amount
   .filter(item => item > 0)
   .reduce((acc , item) => (acc += item) , 0).toFixed(2)
--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -18,8 +18,16 @@ export const GlobalContext = createContext(initialState)
 export const GlobalProvider = ({ children })=>{
     const [state , dispatch] = useReducer(AppReducer , initialState)
 
+    function deleteTransaction(id){
+        dispatch({
+            type: "DELETE_TRANSACTION",
+            payload: id
+        })
+    }
+
     return <GlobalContext.Provider value={{
-        transactions: state.transactions
+        transactions: state.transactions,
+        deleteTransaction
     }}>
 
         {children}
